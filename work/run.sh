58bffab30c862161ed99509fac69930782b649e1
hiiaka <fzk03445@gmail.com>
2022-07-22 13:53:44 +0900

    add config

--- work/run.sh
+++ work/run.sh
@@ -8,15 +8,15 @@ source ./env.sh
 export ROOTFS=${XILINX}/common_image/2022.1/xilinx-zynqmp-common-v2022.1
 export SRC='../basic/krnl_add_cyclic/src'
 export SRCNAME='krnl_add_cyclic'
-export TARGET='sw_emu'
-#export TARGET='hw'
+#export TARGET='sw_emu'
+export TARGET='hw'
 
 mkdir work
 pushd work
 
 aarch64-linux-gnu-g++ -Wall -g -std=c++11 ../${SRC}/main.cpp -o app.exe -I${SYSROOT}/usr/include/xrt -L${SYSROOT}/usr/lib -lOpenCL -lpthread -lrt -lstdc++ --sysroot=${SYSROOT}
-v++ -c -t ${TARGET} --platform ${DEVICE} -k ${SRCNAME} -I../${SRC} ../${SRC}/${SRCNAME}.cpp -o ${SRCNAME}.xo
-v++ -l -t ${TARGET} --platform ${DEVICE} ./${SRCNAME}.xo -o ${SRCNAME}.xclbin
-v++ -p -t ${TARGET} --platform ${DEVICE} ./${SRCNAME}.xclbin --package.out_dir package --package.rootfs ${ROOTFS}/rootfs.ext4 --package.kernel_image ${ROOTFS}/Image
+v++ -c -t ${TARGET} --platform ${DEVICE} -k ${SRCNAME} -I../${SRC} ../${SRC}/${SRCNAME}.cpp -o ${SRCNAME}.xo --config ../run.cfg
+v++ -l -t ${TARGET} --platform ${DEVICE} ./${SRCNAME}.xo -o ${SRCNAME}.xclbin --config ../run.cfg
+v++ -p -t ${TARGET} --platform ${DEVICE} ./${SRCNAME}.xclbin --package.out_dir package --package.rootfs ${ROOTFS}/rootfs.ext4 --package.kernel_image ${ROOTFS}/Image --config ../run.cfg
 
 popd
